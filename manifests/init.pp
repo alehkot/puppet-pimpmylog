9a33aa86bdb11b3850842183f39905ecdcf13614
Aleh Kot <Aleh_Kot@epam.com>
2014-05-24 09:48:20 +0300

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,19 @@
+class pimpmylog(
+  $webroot_location = '/var/www'
+) {
+
+  $location = "$webroot_location/pimpmylog"
+
+  exec { 'pimpmylog-install':
+    command => "git clone https://github.com/potsky/PimpMyLog.git ${location}",
+    creates => "$location",
+    onlyif  => "test ! -d ${location}"
+  }
+
+  file { $location:
+    replace => no,
+    ensure  => directory,
+    mode    => 775
+  }
+
+}
